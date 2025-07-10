fa2e71a3787019e47073639614dd00e1b8268b2f
Himadry Mondol <himadrymondol81@gmail.com>
2025-07-11 04:07:41 +0600

    learning java script

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log('HELLOW');
+console.log("I LIKE SOMETHING");
+let myname="himadry";
+let myclass="bsc 3rd year"
+let attendence=true;
+document.getElementById('name').textContent=`your name is ${myname}`;
+document.getElementById("class").textContent=myclass;
+document.getElementById("present").textContent=attendence;
+let student1,student2,student3,student4;
+student1=10;
+student2=student1+1;
+student3=student1*student2;
+student4=student3/student1;
+document.getElementById('id1').textContent=`the number of student ${student1}`;
+document.getElementById('id2').textContent=`the number of student2 ${student2}`;
+document.getElementById('id3').textContent=`the number of student3 ${student3}`;
+document.getElementById('id4').textContent=`the number of student4 ${student4}`;
