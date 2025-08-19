6a456edb27f5ec04a21d90174b292c28c7a48852
Himadry Mondol <himadrymondol81@gmail.com>
2025-08-20 04:01:54 +0600

    done only 1 thing but not got fully

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let a=10;
-
 a+=10
-
-
-
 console.log(a);
+let username;
+document.getElementById("username").onclick=function(){
+    //here we will connect all that we want to be done by this i mean all everything
+   username=document.getElementById("user").value;
+    console.log(username);
+}
