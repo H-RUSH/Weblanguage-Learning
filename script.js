1f6b625cafad38b0985406faba801b77e14ba8d9
Himadry Mondol <himadrymondol81@gmail.com>
2025-08-21 01:16:26 +0600

    now im learning convertion

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let a=10;
 a+=10
 console.log(a);
 let username;
-document.getElementById("username").onclick=function(){
+document.getElementById(`username`).onclick=function(){
     //here we will connect all that we want to be done by this i mean all everything
-   username=document.getElementById("user").value;
-    console.log(username);
+   username=document.getElementById(`user`).value;
+   document.getElementById(`dipto`).textContent=`hi ${username}`;
 }
