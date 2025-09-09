8e2f209f3512f3afe9642b7ac8d6578cd55aa98f
Himadry Mondol <himadrymondol81@gmail.com>
2025-09-10 02:26:49 +0600

    this is counter program

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
-let a=10;
-a+=10
-console.log(a);
-let username;
-document.getElementById(`username`).onclick=function(){
-    //here we will connect all that we want to be done by this i mean all everything
-   username=document.getElementById(`user`).value;
-   document.getElementById(`dipto`).textContent=`hi ${username}`;
-}
+console.log(`hellow world`);
+
+document.getElementById('welcome').textContent=`hellow world`;
+
+
+document.getElementById('constTxt').textContent='this is constant';
+// here we r doing radios of the circle so we gonna make a box and use pi as constand 
+ const  PI = 3.1416;
+ let radios;
+ let area;
+ document.getElementById(`submit`).onclick=function(){
+    radios=document.getElementById(`radious`).value;
+    console.log(radios , typeof(radios))
+   radios=Number(radios);
+   console.log(radios,typeof(radios));
+    
+  
+ }
+
+ let c_number = 0;
+ let count = document.getElementById(`count`);
+ let incriment =document.getElementById(`count1`);
+ let reset =document.getElementById(`count2`);
+ let decrimentt =document.getElementById(`count3`);
+ count.textContent=c_number;
+ incriment.onclick=function(){
+  c_number++;
+  count.textContent= c_number;
+ }
+ decrimentt.onclick=function(){
+  c_number--;
+  count.textContent= c_number;
+ }
+ reset.onclick=function(){
+  c_number=0;
+  count.textContent= c_number;
+ }
